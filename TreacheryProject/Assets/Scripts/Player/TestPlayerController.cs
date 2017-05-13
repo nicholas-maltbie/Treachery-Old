43f710cfcf960156557cd8b0417dcb5c1ab60f73
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-05-13 00:36:04 -0400

    started work on test player controller

--- TreacheryProject/Assets/Scripts/Player/TestPlayerController.cs
+++ TreacheryProject/Assets/Scripts/Player/TestPlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TestPlayerController : MonoBehaviour {
 
+	public GameObject camera;
+	private float walkSpeed = 4.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,10 +14,18 @@ public class TestPlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//basic movement vector
+		Vector3 movement = Vector3.zero;
 
-		if (Input.GetButtonDown ("Forward")) {
-
+		if (Input.GetAxis("Vertical") == 1) {
+			//move forward
 		}
+		else if (Input.GetAxis ("Vertical") == -1) {
+			//move backward
+		}
+
+		//Move the gameobject movement in the direction of camera times
+		//  walkspeed times deltaTime.
 		
 	}
 }
