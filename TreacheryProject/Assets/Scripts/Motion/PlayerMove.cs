7d31f29685098c7b11fbe08a2dfde25c74dad662
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-05-17 22:57:14 -0400

    Added ability for character to jump

--- TreacheryProject/Assets/Scripts/Motion/PlayerMove.cs
+++ TreacheryProject/Assets/Scripts/Motion/PlayerMove.cs
@@ -8,14 +8,26 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour {
 
 	public float moveSpeed = 1.0f;
+	public float jumpSpeed = 3.0f;
+
 	public bool canMove = true;
+	public bool canJump = true;
 
-	private bool isOnGround = true;
 	private float gravity = 1.0f;
+	private bool prevGround = true;
 
+	public Rigidbody characterRigidbody;
 	public Transform characterTransform;
 	public Animator characterAnimator;
+	public Collider characterCollider;
 
+	bool IsGrounded() {
+		RaycastHit hit;
+
+		if (Physics.Raycast (characterTransform.position, -Vector3.up, out hit))
+			return hit.distance <= 0.1f;
+		return false;
+	}
 	// Update is called once per frame
 	void Update () {
 		//Init move vector to zero
@@ -23,8 +35,18 @@ public class PlayerMove : MonoBehaviour {
 		//Get vertical and horizontal movement.
 		float dz = Input.GetAxis ("Vertical");
 		float dx = Input.GetAxis ("Horizontal");
+		float jump = Input.GetAxis ("Jump");
+
+		if (characterAnimator.GetBool ("jump") && IsGrounded ()) {
+			characterAnimator.SetBool ("jump", false);
+		}else if (jump == 1 && canJump && IsGrounded ()) {
+			characterAnimator.SetBool ("jump", true);
+			characterRigidbody.velocity = new Vector3 (characterRigidbody.velocity.x, 
+				jumpSpeed, characterRigidbody.velocity.z);
+		}
+
 		//Move the player if he or she can move
-		if (canMove && isOnGround) {
+		if (canMove) {
 			//Add vertical movement
 			move += dz * characterTransform.forward;
 			//Add horizontal movement
@@ -33,6 +55,7 @@ public class PlayerMove : MonoBehaviour {
 			//Set movement distance to movespeed * time elapsed
 			move = move.normalized * moveSpeed * Time.deltaTime;
 		}
+
 		//set animator value walking to true (just for now though)
 		characterAnimator.SetBool("walking", Mathf.Abs(dz) + Mathf.Abs(dx) > 0);
 		//Set animator vx and vz
@@ -41,5 +64,8 @@ public class PlayerMove : MonoBehaviour {
 
 		//Translate character based on move.
 		characterTransform.position += move;
+
+		//Save the previous grounded state of the character
+		prevGround = IsGrounded ();
 	}
 }
