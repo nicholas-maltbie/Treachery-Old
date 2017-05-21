90742fa9b45c1230275080c3ade74bd443a294d2
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-05-21 00:49:33 -0400

    Corrected jumping and animations, started work on footsteps

--- TreacheryProject/Assets/Scripts/Motion/PlayerMove.cs
+++ TreacheryProject/Assets/Scripts/Motion/PlayerMove.cs
@@ -6,22 +6,58 @@ using UnityEngine;
  * This script manages how a player can move
  */
 public class PlayerMove : MonoBehaviour {
-
+	/**
+	 * Speed that the character moves
+	 */
 	public float moveSpeed = 1.0f;
+	/**
+	 * Velocity the character takes off from the ground at
+	 */
 	public float jumpSpeed = 3.0f;
+	/**
+	 * Minimum ground time between jumps
+	 */
 	public float minDownTimeBeforeJump = 1.0f;
 
+	/**
+	 * Can the player currently move
+	 */
 	public bool canMove = true;
+	/**
+	 * Can the player currently jump
+	 */
 	public bool canJump = true;
-	
-	private float groundTime = 0.0f;
-	private bool prevGround = true;
 
+	/**
+	 * How long has the character been on the ground
+	 */
+	private float groundTime = 0.0f;
+
+	/**
+	 * Character's rigidbody
+	 */
 	public Rigidbody characterRigidbody;
+	/**
+	 * Character base that moves
+	 */
 	public Transform characterTransform;
+	/**
+	 * Character animator, for animating the character.
+	 * The animator must have the following parameters.
+	 * 	vx - x velocity
+	 *  vz - z velocity
+	 *  walking - Is the character walking
+	 *  jump - Is the character currently jumping/airborne
+	 */
 	public Animator characterAnimator;
+	/**
+	 * Character collider
+	 */
 	public CapsuleCollider characterCollider;
 
+	/**
+	 * Function to check if the character is currently grounded
+	 */
 	bool IsGrounded() {
 		RaycastHit hit;
 		if (Physics.SphereCast(characterTransform.position + new Vector3(0, characterCollider.height, 0), 
@@ -38,21 +74,29 @@ public class PlayerMove : MonoBehaviour {
 		//Get vertical and horizontal movement.
 		float dz = Input.GetAxis ("Vertical");
 		float dx = Input.GetAxis ("Horizontal");
+		//does the player want to jump
 		float jump = Input.GetAxis ("Jump");
+		//check if the player is grounded
 		bool grounded = IsGrounded ();
 
-
+		//If the player was jumping due to being airbone and is now on the ground
+		// tell the animator that the character is on the ground
 		if (characterAnimator.GetBool ("jump") && grounded) {
 			characterAnimator.SetBool ("jump", false);
-		}else if (jump == 1 && canJump && grounded && groundTime >= minDownTimeBeforeJump) {
+		}
+		//If the player wants to jump, is permitted to jump,
+		// is on the gorund and has waited for the cooldown to to wear off, jump
+		else if (jump == 1 && canJump && grounded && groundTime >= minDownTimeBeforeJump) {
 			characterAnimator.SetBool ("jump", true);
 			characterRigidbody.velocity = new Vector3 (characterRigidbody.velocity.x, 
 				jumpSpeed, characterRigidbody.velocity.z);
 		}
 
+		//If the player is grounded, update ground time.
 		if (grounded) {
 			groundTime += Time.deltaTime;
 		} else {
+			//If not grounded, set ground time to zero.
 			groundTime = 0;
 		}
 
@@ -75,8 +119,5 @@ public class PlayerMove : MonoBehaviour {
 		//Set animator vx and vz
 		characterAnimator.SetFloat ("vx", dx);
 		characterAnimator.SetFloat ("vz", dz);
-
-		//Save the previous grounded state of the character
-		prevGround = IsGrounded ();
 	}
 }
---
+++ TreacheryProject/Assets/Scripts/Player/FootSounds.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Character foot sounds, sounds when the charactr's foot does stuff.
+ * Supported events - 
+ * 	Lift - When the player jumps
+ *  Land - When the player lands
+ *  FootDown - Whenever a foot hits the ground
+ * 
+ * Each of these sounds can have a different sound based on the material.
+ */
+public class FootSounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Lift() {
+
+	}
+
+	void Land() {
+
+	}
+
+	void LFootDown() {
+
+	}
+
+	void RFootDown() {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
