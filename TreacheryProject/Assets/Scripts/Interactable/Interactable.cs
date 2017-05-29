195258322218319ab447363c2ae60d37e19e7914
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-05-29 12:47:28 -0400

    Imported door asset along with many assets from previous game

---
+++ TreacheryProject/Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+/// <summary>
+/// An object that the player can interact with.
+/// </summary>
+public class Interactable : NetworkBehaviour {
+	/// <summary>
+	/// The icons.
+	/// </summary>
+	public List<Texture> icons = new List<Texture>();
+	/// <summary>
+	/// The messages that correspond with the respective icons.
+	/// </summary>
+	public List<string> messages = new List<string>();
+
+	/// <summary>
+	/// The distace that a player must be within in order to interact with this object (in unity units).
+	/// </summary>
+	public float useDistance = 2;
+	/// <summary>
+	/// The script that is called when the object is interacted with.
+	/// </summary>
+	public GameObject actionScript;
+	/// <summary>
+	/// The current interaction message to determine the icon.
+	/// </summary>
+	[SyncVar]
+	public string interactionMessage;
+	/// <summary>
+	/// Can this object be interacted with.
+	/// </summary>
+	[SyncVar]
+	public bool canInteract = true;
+
+	/// <summary>
+	/// Interacts the with object.
+	/// </summary>
+	/// <param name="player">Player who is giving the command.</param>
+	[ServerCallback]
+	public void InteractWithObject (GameObject player)
+	{
+		actionScript.SendMessage ("Interact", player);
+	}
+
+	/// <summary>
+	/// Gets the current icon.
+	/// </summary>
+	/// <returns>The icon or null if the current interaction message has no corresponding icon.</returns>
+	public Texture GetIcon()
+	{
+		if (HasIcon()) {
+			return icons[messages.IndexOf (interactionMessage)];
+		}
+		return null;
+	}
+
+	/// <summary>
+	/// Determines whether this instance has icon for its current message.
+	/// </summary>
+	/// <returns><c>true</c> if this instance has icon; otherwise, <c>false</c>.</returns>
+	public bool HasIcon()
+	{
+		return messages.Contains(interactionMessage);
+	}
+}
