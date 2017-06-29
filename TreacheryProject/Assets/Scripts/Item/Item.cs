e62f4ab44e53961e549540b7faf6982e2765f5bd
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-06-29 00:14:52 -0400

    Made latern usable (can turn on and off)

--- TreacheryProject/Assets/Scripts/Item/Item.cs
+++ TreacheryProject/Assets/Scripts/Item/Item.cs
@@ -2,11 +2,20 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class Item : MonoBehaviour {
+public class Item : NetworkBehaviour {
 	
 	public string itemName;
 	public float mass = 1, drag = 0, angularDrag = 0.05f;
 	public bool useGravity = true, isHeld;
+
+	[ServerCallback]
+	public void ServerUse(GameObject holder) {
+		gameObject.SendMessage ("Use", holder);
+	}
+
+	public void Use(GameObject holder) {
+		// Do something
+	}
 
 	public void Interact (GameObject actor)
 	{
--- TreacheryProject/Assets/Scripts/Player/GamePlayer.cs
+++ TreacheryProject/Assets/Scripts/Player/GamePlayer.cs
@@ -8,11 +8,19 @@ public class GamePlayer : NetworkBehaviour {
 	private Actor playerActor;
 	private PlayerMove playerMove;
 	private Inventory inventory;
+	public bool canSwitch;
+
+	private bool prevHeld;
 
 	void Start() {
 		playerActor = GetComponent<Actor> ();
 		playerMove = GetComponent<PlayerMove> ();
 		inventory = GetComponent<Inventory> ();
+	}
+
+	[Command]
+	public void CmdUse() {
+		inventory.held.GetComponent<Item> ().ServerUse (gameObject);
 	}
 
 	void Update() {
@@ -21,20 +29,25 @@ public class GamePlayer : NetworkBehaviour {
 				playerActor.InteractWithObject ();
 			}
 
-			if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
-				inventory.selected += 1;
-				if (inventory.selected >= inventory.items.Length)
-					inventory.selected = 0;
+			if (canSwitch) {
+				if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+					inventory.selected += 1;
+					if (inventory.selected >= inventory.items.Length)
+						inventory.selected = 0;
+				} else if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+					inventory.selected -= 1;
+					if (inventory.selected < 0)
+						inventory.selected = inventory.items.Length - 1;
+				}
 			}
-			else if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
-				inventory.selected -= 1;
-				if (inventory.selected < 0)
-					inventory.selected = inventory.items.Length - 1;
-			}
-
 			if (Input.GetButton ("Drop") && inventory.IsHoldingItem()) {
 				inventory.DropItem (inventory.selected);
 			}
+
+			if (Input.GetButton ("Use") && inventory.IsHoldingItem () && !prevHeld) {
+				CmdUse ();
+			}
+			prevHeld = Input.GetButton ("Use");
 		}
 	}
 }
